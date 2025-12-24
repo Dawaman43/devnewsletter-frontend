bfccb1bd407a0cd76f0378677e108e15c93ff585
Dawit Worku <dawitworkujima@gmail.com>
2025-12-24 15:57:08 +0300

    feat: Implement server-side markdown rendering, add post feedback and view tracking, and introduce a search page.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,17 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import icon from 'astro-icon'
 import node from '@astrojs/node'
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
-    vite:{
+    site: 'http://localhost:4321', // Replace with your production domain
+    vite: {
         plugins: [tailwindcss()],
-        
+
     },
-    integrations: [icon()],
+    integrations: [icon(), sitemap()],
     output: 'server',
-    adapter: node({mode: 'standalone' }),
+    adapter: node({ mode: 'standalone' }),
 });
